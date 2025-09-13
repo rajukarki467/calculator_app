5b45b5c26384235ad0487fad3898c35cd8c4ad53
rajukarki467 <rajukarki467@gmail.com>
2025-09-13 16:43:48 +0545

    making a output field

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -10,6 +10,30 @@ class CalculatorScreen extends StatefulWidget {
 class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            //output
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "0",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 48),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+              ),
+            ),
+
+            //button
+          ],
+        ),
+      ),
+    );
   }
 }
